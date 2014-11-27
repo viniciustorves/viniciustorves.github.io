e7e0313643d91bdc84d6766308a0894803af869a
Bruno Sanches <bsanches@MIA-NOT-004.tray.intranet>
2014-11-27 17:49:09 -0200

    teste

--- tema-mobile.js
+++ tema-mobile.js
@@ -64,10 +64,10 @@
                 $('html, body').scrollTop(0);
                 
                 if($('#NavLateralEsquerda').css('display') == "none") {
-                    $('.hamburguer').css('background-image','https://images.tcdn.com.br/img/arquivos/mobile/images/mobile/hamburguer-tema-x.gif?v=4');
+                    $('.hamburguer').css('background','https://images.tcdn.com.br/img/arquivos/mobile/images/mobile/hamburguer-tema-x.gif?v=4');
                 }
                 else {
-                     $('.hamburguer').css('background-image','https://images.tcdn.com.br/img/arquivos/mobile/images/mobile/hamburguer-tema.png');
+                     $('.hamburguer').css('background','https://images.tcdn.com.br/img/arquivos/mobile/images/mobile/hamburguer-tema.png');
                 }
                 
                 $('#NavLateralEsquerda').toggle('fast');
