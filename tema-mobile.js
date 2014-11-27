87112d9ee8fa4c9a00e4591077f573527657ab2b
Bruno Sanches <bsanches@MIA-NOT-004.tray.intranet>
2014-11-27 10:20:29 -0200

    teste

--- tema-mobile.js
+++ tema-mobile.js
@@ -141,7 +141,7 @@
             $('p').css('display', 'block');
             $('p').css('padding', '10px 0');
             $('.tablePage img').css('display', 'none');
-            $('.tablePage tr th nth-child(4)').css('display', 'none');
+            $('.tablePage tr th:nth-child(4)').css('display', 'none');
             
 
             
