c82188ef5d4f9b1be9222e791c8b4ef73fa4db6a
Bruno Sanches <bsanches@tray.net.br>
2014-11-28 16:36:23 -0200

    teste

--- tema-mobile.js
+++ tema-mobile.js
@@ -5,6 +5,7 @@
 (function($) {
 
     $('head').append('<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">');
+    $('head').append('<meta name="mobileoptimized" content="0" />');
 
 
 
