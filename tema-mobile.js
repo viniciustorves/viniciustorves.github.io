a5d4fa6d12b4d79f84c9741dbd5e74517fcd3831
Bruno Sanches <bsanches@tray.net.br>
2014-12-01 17:51:23 -0200

    teste

--- tema-mobile.js
+++ tema-mobile.js
@@ -74,9 +74,11 @@
 
                 if ($('#NavLateralEsquerda').css('display') == "none") {
                     $('.hamburguer').css('background', 'url(https://images.tcdn.com.br/img/arquivos/mobile/images/mobile/hamburguer-tema-x.png?v=7) center center no-repeat');
+                    $('.hamburguer').addClass('bgcolor');
                 }
                 else {
                     $('.hamburguer').css('background', 'url(https://images.tcdn.com.br/img/arquivos/mobile/images/mobile/hamburguer-tema.png) center center no-repeat');
+                    $('.hamburguer').addClass('bgcolor');
                 }
 
                 $('#NavLateralEsquerda').toggle('fast');
