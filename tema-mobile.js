1f87e642778cc2505911de44986fcd4673c9796a
Bruno Sanches <bsanches@tray.net.br>
2015-01-14 17:40:54 -0200

    teste

--- tema-mobile.js
+++ tema-mobile.js
@@ -235,7 +235,6 @@
             })
             
             var sair = $("li").find("[data-tray-tst='sair']").html();
-            
             $('.central-menu').append('<div class="icoFilho central-icons"> <p>Sair</p>'+sair+'</div>');
             
             
