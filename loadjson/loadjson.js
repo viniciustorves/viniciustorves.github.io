efd1e2a164af34b2026a7493e7bd0ee5cf73044c
bsanches <bsanches@tray.net.br>
2015-05-27 10:24:43 -0300

    teste

--- loadjson/loadjson.js
+++ loadjson/loadjson.js
@@ -152,22 +152,34 @@
         });
     };
 
-    $.fn.serializeObject = function()
-    {
-        var o = {};
-        var a = this.serializeArray();
-        $.each(a, function() {
-            if (o[this.name] !== undefined) {
-                if (!o[this.name].push) {
-                    o[this.name] = [o[this.name]];
-                }
-                o[this.name].push(this.value || '');
-            } else {
-                o[this.name] = this.value || '';
-            }
-        });
-        return o;
-    };
+    $.fn.serializeJSON = function(){
+    var json = {}
+    var form = $(this);
+    form.find('input, select').each(function(){
+      var val
+      if (!this.name) return;
+ 
+      if ('radio' === this.type) {
+        if (json[this.name]) { return; }
+ 
+        json[this.name] = this.checked ? this.value : '';
+      } else if ('checkbox' === this.type) {
+        val = json[this.name];
+ 
+        if (!this.checked) {
+          if (!val) { json[this.name] = ''; }
+        } else {
+          json[this.name] = 
+            typeof val === 'string' ? [val, this.value] :
+            $.isArray(val) ? $.merge(val, [this.value]) :
+            this.value;
+        }
+      } else {
+        json[this.name] = this.value;
+      }
+    })
+    return json;
+  }
 
     
     
